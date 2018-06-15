027dba345bda6e8e4a2c38dd6a8569f761099c95
Saad Aleem <saad@nextgeni.com>
2018-06-15 18:17:35 +0500

    got rid of useless imports in settings

--- stream_service/stream_service/settings.py
+++ stream_service/stream_service/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
 from .local import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
